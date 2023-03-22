ff473f24f572029c34aecf67ca3647d7eeb1417a
Brian <mahuibrian14@gmail.com>
2023-03-22 13:20:21 +0300

    1st commit

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Feet){
   // Write your code here!
+  if (Feet<=400){
+  return ('This one is on me!');
+  }else if ( Feet>2000 && Feet<2500){
+    return ('I will gladly take your thirty bucks.');
+  }else if( Feet>2500){
+    return ('No can do.');}
 }
+//console.log(Feet(2100));
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+
+
+
+
+
+function ternaryCheckCity(NYC){
   // Write your code here!
+   
+  return NYC? "Ok, sounds good.": "No go." ;
+  }
+
+function switchOnCharmFromTip(generous){
+  // Write your code here!
+  
+  switch(generous) {
+    case 'generous': 
+    return ("Thank you so much.");
+    case 'not as generous':
+    return ("Thank you.")
+    default:
+    return ("Bye.")
+  }
 }
